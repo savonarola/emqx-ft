48b8b03e241d00bc12fd1b909a2122feeb17b128
Ilya Averyanov <av@rubybox.dev>
2023-04-03 20:56:49 +0300

    add filename support and use checksum

--- src/emqx_ft/main.py
+++ src/emqx_ft/main.py
@@ -13,6 +13,7 @@ def main():
     parser.add_argument("--port", default=1883, type=int)
     parser.add_argument("--host", default="127.0.0.1", type=str)
     parser.add_argument("--file", required=True, type=argparse.FileType("r"))
+    parser.add_argument("--file-name", required=False, type=str)
     parser.add_argument("--segment-size", default=1024, type=int)
     parser.add_argument("--file-id", required=True, type=str)
     parser.add_argument("--client-id", required=True, type=str)
@@ -24,9 +25,12 @@ def main():
     hash.update(data)
     checksum = hash.hexdigest()
 
+    filename = args.file_name or args.file.name
+
     meta = {
-        "name": args.file.name,
-        "size": len(data)
+        "name": filename,
+        "size": len(data),
+        "checksum": checksum
     }
 
     topic_prefix = f"$file/{args.file_id}"
@@ -38,7 +42,7 @@ def main():
     client.publish(f"{topic_prefix}/init", json.dumps(meta), qos=1)
     for offset, chunk in segments(data, args.segment_size):
         client.publish(f"{topic_prefix}/{offset}", chunk, qos=1)
-    info = client.publish(f"{topic_prefix}/fin", "", qos=1)
+    info = client.publish(f"{topic_prefix}/fin/{len(data)}", "", qos=1)
 
     def on_publish(client, userdata, mid):
         if mid == info.mid:

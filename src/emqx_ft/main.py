0a9b85f1c620496cc23033199b3148bd9748716c
Ilya Averyanov <av@rubybox.dev>
2023-11-24 11:47:29 +0200

    add async & session expiry interval options

--- src/emqx_ft/main.py
+++ src/emqx_ft/main.py
@@ -4,6 +4,7 @@ import json
 import logging
 
 import paho.mqtt.client as mqtt
+from paho.mqtt.packettypes import PacketTypes
 
 
 def main():
@@ -17,6 +18,8 @@ def main():
     parser.add_argument("--segment-size", default=1024, type=int)
     parser.add_argument("--file-id", required=True, type=str)
     parser.add_argument("--client-id", required=True, type=str)
+    parser.add_argument("--session-expiry-interval", default=120, type=int)
+    parser.add_argument("--async", action='store_true')
     args = parser.parse_args()
 
     data = open(args.file.name, "rb").read()
@@ -33,12 +36,16 @@ def main():
         "checksum": checksum
     }
 
-    topic_prefix = f"$file/{args.file_id}"
+    if vars(args).get("async"):
+        topic_prefix = f"$file-async/{args.file_id}"
+    else:
+        topic_prefix = f"$file/{args.file_id}"
 
-    client = mqtt.Client(client_id=args.client_id)
-    client.protocol_version = mqtt.MQTTv5
+    client = mqtt.Client(client_id=args.client_id, protocol=mqtt.MQTTv5)
     client.enable_logger(logging.getLogger())
-    client.connect(args.host, args.port, 60)
+    properties = mqtt.Properties(PacketTypes.CONNECT)
+    properties.SessionExpiryInterval = args.session_expiry_interval
+    client.connect(args.host, args.port, 60, properties=properties)
     client.publish(f"{topic_prefix}/init", json.dumps(meta), qos=1)
     for offset, chunk in segments(data, args.segment_size):
         client.publish(f"{topic_prefix}/{offset}", chunk, qos=1)
